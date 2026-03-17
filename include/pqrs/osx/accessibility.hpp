977bcaa8641cc5b94c14d736fcfb343e19649a70
Takayama Fumihiko <tekezo@pqrs.org>
2026-03-17 23:31:37 +0900

    http -> https

--- include/pqrs/osx/accessibility.hpp
+++ include/pqrs/osx/accessibility.hpp
@@ -4,7 +4,7 @@
 
 // (C) Copyright Takayama Fumihiko 2021.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "accessibility/application.hpp"
 #include "accessibility/focused_ui_element.hpp"
